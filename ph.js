9045a30d292b077c94b433702860b0aa6547ca0a
Drak0ndr <drakondr.video@gmail.com>
2021-07-08 19:03:23 +0700

    Update ph.js

--- ph.js
+++ ph.js
@@ -1,15 +1,17 @@
+console.time('FirstWay')
 const { exec } = require("child_process") 
-
-let photo = setInterval(function() {
-    exec("fswebcam -r 640x360 --no-banner image3.jpg", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-         if (stderr) {
-             console.log(`stderr: ${stderr}`);
-             return;
-         }
-        console.log(`stdout: ${stdout}`);
-    })
-},40)
+// fswebcam -r 640x360 --no-banner image3.jp
+    for(let i = 0; i < 100; i++) {
+        exec("fswebcam -r 640x360 --no-banner image3.jp", (error, stdout, stderr) => {
+            if (error) {
+                //console.log(`error: ${error.message}`);
+                return;
+            }
+             if (stderr) {
+                 //console.log(`stderr: ${stderr}`);
+                 return;
+             }
+            //console.log(`stdout: ${stdout}`);
+        })
+    }
+    console.timeEnd('FirstWay')
