3cb0752d207f8a1e9fcee11eb4b9af53b5841ebe
Drak0ndr <drakondr.video@gmail.com>
2021-07-09 19:21:18 +0700

    test

---
+++ appLong.js
@@ -0,0 +1,49 @@
+var fs = require('fs')
+var http = require('http')
+var img = 'powered by Drakondr'
+var sysState = {}
+const si = require('systeminformation')
+var update = setInterval(function() {
+	//console.time('image')
+	img = JSON.stringify(fs.readFileSync('Test.jpg', 'base64'))
+	//console.timeEnd('image')
+//console.log('update')
+},40)
+
+
+http.createServer(function(req, res) {
+	console.time('serv')
+	res.writeHead(200, { 
+		'Content-Type': 'text/plain',
+		'Access-Control-Allow-Origin': '*'
+	});
+		res.end(img)
+	  console.timeEnd('serv')
+	//console.log('request')
+}).listen(3000, '0.0.0.0')
+
+http.createServer(function(req, res) {
+	res.writeHead(200, { 
+		'Content-Type': 'text/plain',
+		'Access-Control-Allow-Origin': '*'
+	});
+  	res.end(JSON.stringify(sysState));
+}).listen(3001, '0.0.0.0')
+console.log('Сервер работает')
+function updateSysState() {
+	si.battery()
+  .then(data => {
+	sysState.battery = data.percent
+	//console.log(sysState)
+	//console.log(data)
+	})
+
+	si.cpuTemperature()
+  .then(data => {
+	  sysState.cpuTemp = data.main
+	  //console.log(data)
+	})
+	console.log(sysState)
+}
+updateSysState()
+var telUpdate = setInterval(updateSysState, 1000)
