998f93497bd3a0a2ffb37620276f3a21f25932fe
Drak0ndr <67355664+Drak0ndr@users.noreply.github.com>
2021-10-14 17:06:59 +0700

    Create canvas

---
+++ canvas.js
@@ -0,0 +1,6 @@
+const canv = document.querySelector('.gps')
+const cts = canv.getContext('2d')
+canv.width = innerWidth /100 *15
+canv.height = innerWidth/100 *20
+mm = innerWidth /100 *15/1810
+console.log(mm)
