9d77f9cd010fbc7a28df2e24ed28d943c4f408e4
Drak0ndr <67355664+Drak0ndr@users.noreply.github.com>
2021-10-22 21:18:40 +0700

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,6 +1,27 @@
 const canv = document.querySelector('.gps')
-const cts = canv.getContext('2d')
+const ctx = canv.getContext('2d')
 canv.width = innerWidth /100 *15
 canv.height = innerWidth/100 *20
-mm = innerWidth /100 *15/1810
-console.log(mm)
+mm = innerWidth /100 *15/1850
+console.log(mm, canv.height/mm)
+ctx.fillStyle = 'white'
+ctx.fillRect(0, 0, canv.width, canv.height)
+ctx.beginPath();
+ctx.fillStyle = 'black'
+ctx.moveTo(530*mm + 20*mm, 100*mm);
+ctx.lineTo(530*mm  + 20*mm + 750*mm, 100*mm)
+ctx.lineTo(2*530*mm  + 20*mm + 750*mm, 100*mm + 530*mm)
+ctx.lineTo(2*530*mm  + 20*mm + 750*mm, 100*mm + 530*mm + 1500*mm)
+ctx.lineTo(20*mm, 100*mm + 530*mm + 1500*mm)
+ctx.lineTo(20*mm, 100*mm + 530*mm)
+ctx.closePath()
+ctx.stroke()
+ctx.moveTo(530*mm + 20*mm, 100*mm);
+ctx.lineTo(530*mm  + 20*mm, 100*mm + 530*mm + 1500*mm)
+ctx.stroke();
+ctx.moveTo(530*mm + 20*mm + 750*mm, 100*mm);
+ctx.lineTo(530*mm  + 20*mm + 750*mm, 100*mm + 530*mm + 1500*mm)
+ctx.stroke()
+ctx.moveTo(20*mm, 100*mm + 530 *mm)
+ctx.lineTo(2*530*mm  + 20*mm + 750*mm, 100*mm + 530 *mm)
+ctx.stroke()
